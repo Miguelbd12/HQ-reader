3764a172e509deb903563969aee2feab67234058
Miguelbd12 <miguel.diaz@headquarters.co>
2025-05-12 10:45:21 -0700

    Update HQ reader.py

--- HQ reader.py
+++ HQ reader.py
@@ -21,8 +21,7 @@ uploaded_files = st.file_uploader("Choose invoice PDFs", type=["pdf"], accept_mu
 US_STATES = [
     "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS",
     "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY",
-    "NC", "ND", "OH", "OK", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV",
-    "WI", "WY"
+    "NC", "ND", "OH", "WA", "WV"
 ]
 
 def process_image(image):
